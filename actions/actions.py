c005f56ca04352b41411851e82f1493a6f40804d
ashus3868 <ashus3868@gmail.com>
2020-11-24 13:33:07 +0530

    RasaOpenSource initial comit

---
+++ actions/actions.py
@@ -0,0 +1,62 @@
+# This files contains your custom actions which can be used to run
+# custom Python code.
+#
+# See this guide on how to implement these action:
+# https://rasa.com/docs/rasa/custom-actions
+
+
+# This is a simple example for a custom action which utters "Hello World!"
+
+from typing import Any, Text, Dict, List
+
+from rasa_sdk import Action, Tracker
+from rasa_sdk.events import SlotSet, EventType
+from rasa_sdk.executor import CollectingDispatcher
+import webbrowser
+
+class ActionVideo(Action):
+    def name(self) -> Text:
+        return "action_video"
+
+    async def run(
+        self,
+        dispatcher,
+        tracker: Tracker,
+        domain: "DomainDict",
+    ) -> List[Dict[Text, Any]]:
+        video_url="https://youtu.be/jj4BL9o3Q7o"
+        dispatcher.utter_message("wait... Playing your video.")
+        webbrowser.open(video_url)
+        return []
+
+class ValidateRestaurantForm(Action):
+    def name(self) -> Text:
+        return "user_details_form"
+
+    def run(
+        self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict
+    ) -> List[EventType]:
+        required_slots = ["name", "number"]
+
+        for slot_name in required_slots:
+            if tracker.slots.get(slot_name) is None:
+                # The slot is not filled yet. Request the user to fill this slot next.
+                return [SlotSet("requested_slot", slot_name)]
+
+        # All slots are filled.
+        return [SlotSet("requested_slot", None)]
+
+class ActionSubmit(Action):
+    def name(self) -> Text:
+        return "action_submit"
+
+    def run(
+        self,
+        dispatcher,
+        tracker: Tracker,
+        domain: "DomainDict",
+    ) -> List[Dict[Text, Any]]:
+        dispatcher.utter_message(template="utter_details_thanks",
+                                 Name=tracker.get_slot("name"),
+                                 Mobile_number=tracker.get_slot("number"))
+
